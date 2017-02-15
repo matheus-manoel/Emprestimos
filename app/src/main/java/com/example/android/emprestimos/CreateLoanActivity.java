db49a4a6f5ace1ebf22ab9abee0eb330296875c5
Matheus Manoel <matheusmanoel@MacBook-Air-de-Matheus.local>
2017-02-15 20:46:55 -0200

    First commit.

---
+++ app/src/main/java/com/example/android/emprestimos/CreateLoanActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.emprestimos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateLoanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_loan);
+    }
+}
