3c0f080ec1cb6d55606233e9496d3192352e2c99
Matheus Manoel <matheuscmanoel@gmail.com>
2017-02-19 03:19:55 -0300

    Fix add.

--- app/src/main/java/com/example/android/emprestimos/PeopleFragment.java
+++ app/src/main/java/com/example/android/emprestimos/PeopleFragment.java
@@ -2,6 +2,7 @@ package com.example.android.emprestimos;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,6 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class PeopleFragment extends Fragment {
-
 
     public PeopleFragment() {
         // Required empty public constructor
