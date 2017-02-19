351a31fe7b2b64e14c8a6bad79647420cd5b3efd
Matheus Manoel <matheuscmanoel@gmail.com>
2017-02-19 15:39:07 -0300

    Add click listener to fab.

--- app/src/main/java/com/example/android/emprestimos/MainActivity.java
+++ app/src/main/java/com/example/android/emprestimos/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.emprestimos;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private FloatingActionButton addFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        addFab = (FloatingActionButton) findViewById(R.id.addFab);
+
+        setListeners();
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -71,5 +78,31 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+    }
+
+    public void setListeners() {
+
+        addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = tabLayout.getSelectedTabPosition();
+
+                switch(position) {
+                    case 0:
+                        // first tab is selected
+                        break;
+                    case 1:
+                        // second tab is selected
+                        break;
+                    case 2:
+                        // third tab is selected
+                        break;
+                    case 3:
+                        // fourth tab is selected
+                        break;
+                }
+            }
+        });
+
     }
 }
