8705d97dc3b7dddca53dd980096e44c481bdeca9
Matheus Manoel <matheuscmanoel@gmail.com>
2017-03-14 16:12:10 -0300

    Add preview gifs to read.me

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Emprestimos is a loan management android app built for learning purposes.
 - List loans made
 - Add person
 - List loans per person
+
+<img src="https://cloud.githubusercontent.com/assets/7307341/23918079/4f14e00e-08d0-11e7-8f2c-5876f2ccb02a.gif" width="45%"></img> <img src="https://cloud.githubusercontent.com/assets/7307341/23918083/5247117a-08d0-11e7-8381-df732692ca9a.gif" width="45%"></img>
--- app/src/main/java/com/example/android/emprestimos/database/SQLScripts.java
+++ app/src/main/java/com/example/android/emprestimos/database/SQLScripts.java
@@ -8,12 +8,13 @@ public final class SQLScripts {
 
     public static final String CREATE_PEOPLE = "CREATE TABLE people (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
             "name TEXT NOT NULL," +
-            "email TEXT NOT NULL," +
-            "phone TEXT NOT NULL);";
+            "email TEXT," +
+            "phone TEXT);";
 
     public static final String CREATE_LOANS = "CREATE TABLE loans (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
             "loanDate TEXT NOT NULL," +
-            "isMoney TEXT NOT NULL," +
+            "isMoney CHAR NOT NULL," +
+            "value TEXT," +
             "phone TEXT NOT NULL," +
             "loan_person INTEGER NOT NULL," +
             "FOREIGN KEY(loan_person) REFERENCES people(_id));";
